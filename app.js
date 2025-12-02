2d29fe7cefe0c4246098613559676d93b047b440
Trilok Lowanshi <trilokl01c@gmail.com>
2025-12-02 19:25:09 +0530

    Added functions to apply formulas on different cells

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
+// infix function to calculate basic mathematical operations like add, sub, mul, and div
+const inFixFunctions = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+};
+// function to apply infix functions
+const infixEval = (str, regex) =>
+    str.replace(regex, (_, arg1, op, arg2) =>
+        inFixFunctions.op(parseFloat(arg1), parseFloat(arg2))
+    );
+
+// a function to check of precedense
+const highPrecedence = (str) => {
+    // regex to match a infix expression
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const s = infixEval(str, regex);
+    return s === str ? str : highPrecedence(s);
+};
+
+// setting range of cols, vertical numbering
 const range = (start, end) =>
     Array(end - start + 1)
         .fill(start)
         .map((element, index) => element + index);
+// setting range of rows, horizontal alphabating
 const charRange = (start, end) =>
     range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
         String.fromCharCode(code)
     );
 
+// sum function for the whole range of array
 const sum = (nums) => nums.reduce((el, acc) => el + acc, 0);
+// count function
 const count = (nums) => nums.length;
+// even or odd
 const isEven = (num) => !(num % 2);
+// find average of a given range
 const average = (nums) => sum(nums) / nums.length;
+// find median of a given range
 const median = (nums) => {
     const sorted = nums.slice().sort();
     const mid = nums.length / 2 - 1;
@@ -18,6 +46,7 @@ const median = (nums) => {
     else return sorted[Math.ceil(mid)];
 };
 
+// function to evaluate formulas applied
 const evalFormula = (cells, x) => {
     const idToText = (id) => cells.find((cell) => cell.id === id).value;
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
@@ -34,6 +63,8 @@ const spreadsheetFuncs = {
     median,
     count,
 };
+
+// loading window
 window.onload = () => {
     const colHeaders = document.getElementById("col-head");
     const rowHeaders = document.getElementById("row-head");
